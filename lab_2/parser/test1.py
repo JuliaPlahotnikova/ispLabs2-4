cb224701f8d7bb7b9a2461de482f02eb34638b53
Yulechka <packetickchaya@gmail.com>
2021-06-14 15:42:43 +0300

    Move parsers to another dir

---
+++ lab_2/parser/test1.py
@@ -0,0 +1,32 @@
+string = "a+b+c=10"
+list = ["shop", "petshop", 2021]
+
+def tuple_returner(a, b, c, d):
+    x = a + b
+    y = c + d
+    return (x, y)
+
+k=30
+
+class B:
+    a = 666
+    b = [333, 666, 'www']
+
+objects = [{}, set(), tuple(), []]
+
+def func(x):
+    c=90
+    return k + x +c
+
+lmbd = lambda x: x*x^2
+
+class Counter():
+    a = 1
+    def my_func(x):
+        return x
+
+
+dict = {
+  "GROUP": "NIRVANA",
+  "year": 1987
+}
